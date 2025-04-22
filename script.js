4a713e50e3934419b987a02dbe4eb914d8f608ee
Jose Hernandez <josealfredo.066@gmail.com>
2025-04-22 15:40:28 -0700

    ahora el js reconoce a / como index.html

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 const swup = new Swup();
 
 // Función para marcar el enlace activo
-function nose(){
-    const activePage = window.location.pathname;
+function nose() {
+    const activePage = window.location.pathname === '/' ? '/index.html' : window.location.pathname;
+
     document.querySelectorAll('nav a').forEach(link => {
-        if (link.href.includes(`${activePage}`)) {
+        const linkPath = new URL(link.href).pathname;
+
+        if (linkPath === activePage || (linkPath === '/' && activePage === '/index.html')) {
             link.classList.add('active');
         } else {
             link.classList.remove('active');
         }
     });
 }
+
 
 nose(); // Llamar a la función al cargar la página
 
