09d2851558808966ffb5be02ce584ed0c587c004
Jose Hernandez <josealfredo.066@gmail.com>
2025-06-08 18:29:36 -0400

    feat: add About component with styling and integrate into App structure

---
+++ src/components/About/About.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+import styles from './About.module.css'
+import { getImageUrl } from '../../assets/utils';
+
+export const About = () => {
+  return (
+    <section className={styles.container} id='about'>
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem} >
+            <img src={getImageUrl('about/cursoricon.png')} alt='cursor icon'/>
+            <div className={styles.aboutItemText}>
+              <h3>Frontend Developer</h3>
+              <p>
+                Frontend developer with expirience in building responsive and
+                optimized sites
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl('about/servericon.png')} alt='server icon' />
+            <div className={styles.aboutItemText}>
+              <h3>Cybersecurity Technologist</h3>
+              <p>
+                Computer Science student at APEC with a strong foundation in
+                cybersecurity, backed by a degree in Cybersecurity Technology
+                from ITLA.
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
