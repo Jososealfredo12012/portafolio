5967192e4a130149a6f7fe87669e38e253c6d833
Jose Hernandez <josealfredo.066@gmail.com>
2025-06-08 20:01:33 -0400

    feat: add Projects component with ProjectCard for project display

---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import projects from '../../data/projects.json';
+import { ProjectCard } from './ProjectCard';
+
+export const Projects = () => {
+  return (
+    <section>
+      <h2>Projects</h2>
+      <div>
+        {projects.map((project, id) => {
+          return (
+            <ProjectCard key={id} project={project}></ProjectCard>
+          );
+        })}
+      </div>
+    </section>
+  );
+};
