8948474a3f89682b2defc0fbd84990aee66878e5
Jose Hernandez <josealfredo.066@gmail.com>
2025-06-08 20:57:13 -0400

    feat: enhance ProjectCard and Projects components with improved styling and layout

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import projects from '../../data/projects.json';
 import { ProjectCard } from './ProjectCard';
+import styles from './Projects.module.css'
 
 export const Projects = () => {
   return (
-    <section>
-      <h2>Projects</h2>
-      <div>
+    <section className={styles.container} id='projects'>
+      <h2 className={styles.title}>Projects</h2>
+      <div className={styles.project}>
         {projects.map((project, id) => {
           return (
             <ProjectCard key={id} project={project}></ProjectCard>
